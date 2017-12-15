7d37d2790deaec8969467f6ef945a1ff434f7e46
wd <martinwu42@outlook.com>
2017-12-15 20:03:42 +0800

    Hide those navs, boi!

--- batchcheck.py
+++ batchcheck.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
-from check import check_file
+from check import *
 from utils import *
 
 cdname = os.path.dirname(__file__)
 archive_folder = os.path.join(cdname, 'archive', '201712')
 
 if __name__ == '__main__':
+    last_pid = None
     for root, dirs, files in os.walk(archive_folder):
         for file in sorted(files):
-            check_file(os.path.join(root, file))
+            oldest_pid, newest_pid = check_file(os.path.join(root, file))
+
+            # Check missed posts between files
+            if oldest_pid:
+                if (last_pid and oldest_pid > last_pid + 1
+                        and oldest_pid < last_pid + max_missed_pid):
+                    for i in range(last_pid + 1, oldest_pid):
+                        print(i, file, 'N')
+                last_pid = oldest_pid
--- check.py
+++ check.py
@@ -5,22 +5,23 @@ from utils import *
 cdname = os.path.dirname(__file__)
 filename = os.path.join(cdname, 'pkuhole.txt')
 
+max_missed_pid = 100
+
 
 def check_file(filename):
     post_list = list(reversed(read_posts(filename)))
     if not post_list:
-        return
+        return None, None
 
-    last_pid = post_list[0]['pid']
+    last_pid = None
     for post in post_list:
         pid = post['pid']
-        time_str = datetime.fromtimestamp(
-            int(post['timestamp'])).strftime('%Y-%m-%d %H:%M:%S')
-        if pid > last_pid and pid < last_pid + 100:
-            if pid > last_pid + 1:
-                for i in range(last_pid + 1, pid):
-                    print(i, time_str, 'N')
-            last_pid = pid
+        time_str = datetime.fromtimestamp(int(
+            post['timestamp'])).strftime('%Y-%m-%d %H:%M:%S')
+        if last_pid and pid > last_pid + 1 and pid < last_pid + max_missed_pid:
+            for i in range(last_pid + 1, pid):
+                print(i, time_str, 'N')
+        last_pid = pid
         first_line = post['text'].splitlines()[0]
         if first_line == '#DELETED':
             print(pid, time_str, 'D')
@@ -28,6 +29,10 @@ def check_file(filename):
             print(pid, time_str, 'M')
         if post['reply'] >= 0 and post['reply'] != len(post['comments']):
             print(pid, time_str, 'R')
+
+    oldest_pid = post_list[0]['pid']
+    newest_pid = post_list[-1]['pid']
+    return oldest_pid, newest_pid
 
 
 if __name__ == '__main__':
